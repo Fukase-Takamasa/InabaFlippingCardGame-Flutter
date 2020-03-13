d557825e75ff6cf5b028b1fc96f6317485922ca4
深瀬 貴将 <fukase@arsaga.jp>
2020-03-13 18:48:43 +0900

    背景を緑に、セルの縦横比を変更。

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'InabaFlippingCardGame',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.green,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -34,23 +35,31 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: GridView.count(
-        crossAxisCount: 6, //1行に表示する数
-        crossAxisSpacing: 10, //縦スペース
-        mainAxisSpacing: 10, //横スペース
-        shrinkWrap: true,
-        children: List.generate(50, (index) {
-          return Container(
-            padding: const EdgeInsets.all(8.0),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.green,
-            ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 30, top: 100, right: 30, bottom: 100),
+        child: GridView.count(
+          //GridView.countはWidget名で、画面に入りきるセル数ではなく、
+          // 自分で指定した(count)セル数を表示する
+          crossAxisCount: 6, //1行に表示する数
+          crossAxisSpacing: 10, //縦スペース
+          mainAxisSpacing: 10, //横スペース
 
-          );
-        }),
+          shrinkWrap: true,
+          children: List.generate(30, (index) { //セル数
+            return Container(
+//            padding: const EdgeInsets.only(top: 20.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
 
+            );
+          }),
+
+        ),
       )
+
     );
   }
 }
