d29788f2c6768f2fbfc6e0ec800b4984707a4a5a
Takamasa Fukase <takama224@yahoo.co.jp>
2020-03-25 09:28:18 +0900

    セル生成をGridView.builderに変更、生成時にif文で表示画像を分岐

--- lib/main.dart
+++ lib/main.dart
@@ -85,28 +85,48 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: EdgeInsets.only(left: 30, top: 100, right: 30, bottom: 100),
-        child: GridView.count(
+        child: GridView.builder(
           //GridView.countはWidget名で、画面に入りきるセル数ではなく、
           // 自分で指定した(count)セル数を表示する
-          crossAxisCount: 6, //1行に表示する数
-          crossAxisSpacing: 10, //縦スペース
-          mainAxisSpacing: 10, //横スペース
-          childAspectRatio: 0.7, //セルの縦横比
-          shrinkWrap: true,
+//          crossAxisCount: 6, //1行に表示する数
+//          crossAxisSpacing: 10, //縦スペース
+//          mainAxisSpacing: 10, //横スペース
+//          childAspectRatio: 0.7, //セルの縦横比
+//          shrinkWrap: true,
 
 //          children: imageList
 
-          children: List.generate(30, (index) { //セル数
-            return Container(
-            padding: const EdgeInsets.all(2.0),
-              child: imageList[index],
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(4),
-              ),
-            );
-          }),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 6,
+            crossAxisSpacing: 10, //縦スペース
+            mainAxisSpacing: 10, //横スペース
+            childAspectRatio: 0.7, //セルの縦横比
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            if (index < inabaCards.length) {
+              if (inabaCards[index].isOpened == "true") {
+                return photoItem(inabaCards[index].imageName);
+              }else {
+                return photoItem("ina2");
+              }
+            }else {
+              return null;
+            }
+          },
+
+
+//          children: List.generate(30, (index) { //セル数
+//            return Container(
+//            padding: const EdgeInsets.all(2.0),
+////              child: imageList[index],
+//
+//              alignment: Alignment.center,
+//              decoration: BoxDecoration(
+//                color: Colors.white,
+//                borderRadius: BorderRadius.circular(4),
+//              ),
+//            );
+//          }),
         ),
       )
     );
