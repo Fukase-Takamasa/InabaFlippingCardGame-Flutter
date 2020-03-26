ea7ff204cf7f2263ec181e6f44c8d382c3777211
Takamasa Fukase <takama224@yahoo.co.jp>
2020-03-27 03:09:12 +0900

    セルをタップで、DBを更新；カードのisOpenedを切り替え。merge; trueを活用

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'cardData.dart';
+//import 'cardData.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'InabaFlippingCardGame≪Flutter≫'),
     );
   }
 }
@@ -68,11 +69,23 @@ class _MyHomePageState extends State<MyHomePage> {
                       return GestureDetector(
                         onTap: () {
                           if (snapshot.data.documents[index]["isOpened"] == false) {
-                            setState(() {
+//                            setState(() {
                               //DBのisOpenedをtrueに更新する
-                            });
+                            Firestore.instance
+                                .collection("currentGameTableData")
+                                .document("cardData${index + 1}")
+                                .setData({
+                              "isOpened": true
+                            }, merge: true);
+//                            });
                           }else if (snapshot.data.documents[index]["isOpened"] == true) {
                               //DBのisOpenedをfalseに更新する
+                            Firestore.instance
+                                .collection("currentGameTableData")
+                                .document("cardData${index + 1}")
+                                .setData({
+                              "isOpened": false
+                            }, merge: true);
                           }
                           print(snapshot.data.documents[index]["imageName"]);
                           print(snapshot.data.documents[index]["isOpened"]);
@@ -95,11 +108,23 @@ class _MyHomePageState extends State<MyHomePage> {
                       return GestureDetector(
                         onTap: () {
                           if (snapshot.data.documents[index]["isOpened"] == false) {
-                            setState(() {
+//                            setState(() {
                               //DBのisOpenedをtrueに更新する
-                            });
+                            Firestore.instance
+                                .collection("currentGameTableData")
+                                .document("cardData${index + 1}")
+                                .setData({
+                              "isOpened": true
+                            }, merge: true);
+//                            });
                           }else if (snapshot.data.documents[index]["isOpened"] == true) {
                             //DBのisOpenedをfalseに更新する
+                            Firestore.instance
+                                .collection("currentGameTableData")
+                                .document("cardData${index + 1}")
+                                .setData({
+                              "isOpened": false
+                            }, merge: true);
                           }
                           print(snapshot.data.documents[index]["imageName"]);
                           print(snapshot.data.documents[index]["isOpened"]);
@@ -117,16 +142,6 @@ class _MyHomePageState extends State<MyHomePage> {
                       );
 //                return photoItem("CardBackImageRed");
                     }
-
-//                    if (index < 30) {
-//                      return photoItem(
-//                          snapshot.data.documents[index]["imageName"]) != null
-//                          ? photoItem(
-//                          snapshot.data.documents[index]["imageName"])
-//                          : Text("Loading...");
-//                    } else {
-//                      return null;
-//                    }
                   }
               );
             }
@@ -144,75 +159,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text(widget.title),
-//      ),
-//      body: Container(
-//        padding: EdgeInsets.only(
-//          top: deviceSize.height * 0.18, bottom: deviceSize.height * 0.2,
-//          left: deviceSize.width * 0.04, right: deviceSize.width * 0.04),
-//        child: GridView.builder(
-//          itemCount: inabaCards.length,
-//          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//            crossAxisCount: 6,
-//            crossAxisSpacing: 10, //縦スペース
-//            mainAxisSpacing: 10, //横スペース
-//            childAspectRatio: 0.7, //セルの縦横比
-//          ),
-//          itemBuilder: (context, index) {
-//            if (inabaCards[index].isOpened == "true") {
-//              print("itemBuilderの中です isOpened trueの方");
-//              return GestureDetector(
-//                onTap: () {
-//                  if (inabaCards[index].isOpened == "false") {
-//                    inabaCards[index].isOpened = "true";
-//                    setState(() {
-//
-//                    });
-//                  }else if (inabaCards[index].isOpened == "true") {
-//                    inabaCards[index].isOpened = "false";
-//                  }
-//                  print(inabaCards[index].imageName);
-//                  print(inabaCards[index].isOpened);
-//                  print(inabaCards[index].isMatched);
-//                },
-//
-//                child: photoItem(inabaCards[index].imageName),
-////                  padding: const EdgeInsets.only(left: 2, right: 2, top: 5, bottom: 5),
-////                  alignment: Alignment.center,
-////                  decoration: BoxDecoration(
-////                    color: Colors.white,
-////                    borderRadius: BorderRadius.circular(4),
-////                  )
-//              );
-//              print("あ");
-//            }else {
-//              print("itemBuilderの中です isOpened falseの方");
-//              return GestureDetector(
-//                onTap: () {
-//                  if (inabaCards[index].isOpened == "false") {
-//                    inabaCards[index].isOpened = "true";
-//                  }else if (inabaCards[index].isOpened == "true") {
-//                    inabaCards[index].isOpened = "false";
-//                  }
-//                  print(inabaCards[index].imageName);
-//                  print(inabaCards[index].isOpened);
-//                  print(inabaCards[index].isMatched);
-//                },
-////                  padding: const EdgeInsets.only(left: 2, right: 2, top: 5, bottom: 5),
-//                child: photoItem("CardBackImageRed"),
-////                  alignment: Alignment.center,
-////                  decoration: BoxDecoration(
-////                    color: Colors.white,
-////                    borderRadius: BorderRadius.circular(4),
-////                  )
-//              );
-////                return photoItem("CardBackImageRed");
-//            }
-//          },
-//        ),
-//      ),
-//    );
