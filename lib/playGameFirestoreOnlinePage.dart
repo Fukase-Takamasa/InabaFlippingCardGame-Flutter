255d21f3949b61e81a48069a7cbebf691bc3dd78
深瀬 貴将 <fukase@arsaga.jp>
2020-04-09 02:28:21 +0900

    ロビー画面のファイル追加、ひとまず最低限のクラス設定だけ記述。

---
+++ lib/playGameFirestoreOnlinePage.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+class PlayGameFirestoreOnlinePage extends StatefulWidget {
+  @override
+  _PlayGameFirestoreOnlinePageState createState() => _PlayGameFirestoreOnlinePageState();
+}
+
+class _PlayGameFirestoreOnlinePageState extends State<PlayGameFirestoreOnlinePage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Lobby"),
+      ),
+    );
+  }
+}
