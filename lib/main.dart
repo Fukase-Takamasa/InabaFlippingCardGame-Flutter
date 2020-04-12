223da3227651c1b4418f694245232eebfef38ed6
深瀬 貴将 <fukase@arsaga.jp>
2020-04-13 00:44:22 +0900

    ロビーのレイアウトのベースを作成

--- lib/main.dart
+++ lib/main.dart
@@ -47,27 +47,65 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         title: Text(widget.title)
       ),
-      body: Center(
-        child: Row(
+      body: Container(
+        padding: EdgeInsets.all(30),
+        child:
+      Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            RaisedButton(
-              child: Text("自分との戦い"),
-              onPressed: () =>  //MyAppのところでroutesに記述したIdentifierを元に遷移
-                  Navigator.of(context).pushNamed("/PlayGameFightWithYourSelf"),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 400,
+              height: 2,
+              color: Colors.white,
             ),
-            RaisedButton(
-              child: Text("オンラインで遊ぶ"),
-              onPressed: () =>  //MyAppのところでroutesに記述したIdentifierを元に遷移
-                  Navigator.of(context).pushNamed("/playGameFirestoreOnline"),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 280,
+              height: 70,
+              child: RaisedButton(
+                child: Text("オンラインで遊ぶ"),
+                onPressed: () =>  //MyAppのところでroutesに記述したIdentifierを元に遷移
+                Navigator.of(context).pushNamed("/playGameFirestoreOnline"),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              ),
             ),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 280,
+              height: 70,
+              child: RaisedButton(
+                child: Text("自分との戦い"),
+                onPressed: () =>  //MyAppのところでroutesに記述したIdentifierを元に遷移
+                Navigator.of(context).pushNamed("/PlayGameFightWithYourSelf"),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 400,
+              height: 2,
+              color: Colors.white,
+            ),
+            Expanded(
+                child: ListView(
+                  padding: EdgeInsets.all(10),
+                children: List.generate(5, (index) {
+                  return Card(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                    child: Container(
+                      margin: EdgeInsets.all(20),
+                      color: Colors.white,
+                      child: Text("ルーム$index"),
+                    ),
+                  );
+                }
+                )
+              )
+            )
           ],
-        )
-//        RaisedButton(
-//          child: Text("オンラインで遊ぶ"),
-//          onPressed: () =>  //MyAppのところでroutesに記述したIdentifierを下に遷移
-//            Navigator.of(context).pushNamed("/playGameFirestoreOnline"),
-//        ),
       ),
+    ),
     );
   }
 }
