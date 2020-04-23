6eb4ab4fe6e24bcafdfc6a674878941b7093d958
深瀬 貴将 <fukase@arsaga.jp>
2020-04-24 04:35:15 +0900

    ロビー画面にFirestore.instanceでsnapshotのstreamを作成

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'playGameFirestoreOnlinePage.dart';
 import 'playGameFightWithYourSelfPage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(MyApp());
 
@@ -50,148 +51,159 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         title: Text(widget.title)
       ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child:
-      Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Stack(
+      body: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance
+              .collection("rooms")
+              .orderBy("defaultRoom").snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const Text("Loading...");
+          }
+          return       Container(
+            padding: EdgeInsets.all(20),
+            child:
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    child: Text(
-                      "ひとりで遊ぶ",
-                      style: TextStyle(
-                        fontSize: 23, fontWeight: FontWeight.bold, color: Colors.white,
+                Stack(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        child: Text(
+                          "ひとりで遊ぶ",
+                          style: TextStyle(
+                            fontSize: 23, fontWeight: FontWeight.bold, color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-            Stack(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child:  Container(
-                    padding: EdgeInsets.all(10),
-                    width: deviceScreenSize.width * 0.83, height: 2, color: Colors.white,
-                  ),
+                Stack(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child:  Container(
+                        padding: EdgeInsets.all(10),
+                        width: deviceScreenSize.width * 0.83, height: 2, color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
 
-            Container(
-              padding: EdgeInsets.all(10),
-              width: deviceScreenSize.width * 0.75, height: 70,
-              child: RaisedButton(
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget> [
-                      Icon(Icons.person_outline),
-                      Text(
-                        "自分との戦い",
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87,
-                      ),
-                      ),
-                    ]
-                ),
-                onPressed: () =>  //MyAppのところでroutesに記述したIdentifierを元に遷移
-                Navigator.of(context).pushNamed("/PlayGameFightWithYourSelf"),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              width: deviceScreenSize.width * 0.75, height: 70,
-              child: RaisedButton(
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget> [
-                    Icon(Icons.desktop_mac),
-                    Text(
-                        "コンピュータと対戦",
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87,
-                        )
+                Container(
+                  padding: EdgeInsets.all(10),
+                  width: deviceScreenSize.width * 0.75, height: 70,
+                  child: RaisedButton(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget> [
+                          Icon(Icons.person_outline),
+                          Text(
+                            "自分との戦い",
+                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87,
+                            ),
+                          ),
+                        ]
                     ),
-                  ]
-                ),
-                onPressed: () =>  //MyAppのところでroutesに記述したIdentifierを元に遷移
-                Navigator.of(context).pushNamed("/PlayGameFightWithYourSelf"),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-              ),
-            ),
-            Stack(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    child: Text(
-                      "オンラインで遊ぶ",
-                      style: TextStyle(
-                        fontSize: 23, fontWeight: FontWeight.bold, color: Colors.white,
-                      ),
-                    ),
+                    onPressed: () =>  //MyAppのところでroutesに記述したIdentifierを元に遷移
+                    Navigator.of(context).pushNamed("/PlayGameFightWithYourSelf"),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
                   ),
                 ),
-              ],
-            ),
-            Stack(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child:  Container(
-                    padding: EdgeInsets.all(10),
-                    width: deviceScreenSize.width * 0.83, height: 2, color: Colors.white,
+                Container(
+                  padding: EdgeInsets.all(10),
+                  width: deviceScreenSize.width * 0.75, height: 70,
+                  child: RaisedButton(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget> [
+                          Icon(Icons.desktop_mac),
+                          Text(
+                              "コンピュータと対戦",
+                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87,
+                              )
+                          ),
+                        ]
+                    ),
+                    onPressed: () =>  //MyAppのところでroutesに記述したIdentifierを元に遷移
+                    Navigator.of(context).pushNamed("/PlayGameFightWithYourSelf"),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
                   ),
                 ),
-              ],
-            ),
-            Expanded(
-              child: Container(
-                width: deviceScreenSize.width * 0.85,
-                  padding: EdgeInsets.only(top: 12, bottom: 12),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10), color: Colors.white,
+                Stack(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        child: Text(
+                          "オンラインで遊ぶ",
+                          style: TextStyle(
+                            fontSize: 23, fontWeight: FontWeight.bold, color: Colors.white,
+                          ),
+                        ),
+                      ),
                     ),
-                    child: ListView(
+                  ],
+                ),
+                Stack(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child:  Container(
+                        padding: EdgeInsets.all(10),
+                        width: deviceScreenSize.width * 0.83, height: 2, color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                    child: Container(
+                      width: deviceScreenSize.width * 0.85,
+                      padding: EdgeInsets.only(top: 12, bottom: 12),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10), color: Colors.white,
+                        ),
+                        child: ListView(
 //                      padding: EdgeInsets.all(10),
-                        children: List.generate(3, (index) {
-                          return GestureDetector(
-                            //タップのメソッド
-                            onTap: () {
-                              Navigator.of(context).pushNamed("/playGameFirestoreOnline");
-                            },
-                            //UIの設定
-                            child: Card(
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                              child: Container(
-                                padding: EdgeInsets.only(left: 10, top: 10, right: 10), height: 40,
-                                color: Colors.white,
-                                child: Text(
-                                  "ルーム${index + 1}（デフォルト）",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.normal,
-                                    color: Colors.black,
+                            children: List.generate(3, (index) {
+                              return GestureDetector(
+                                //タップのメソッド
+                                onTap: () {
+                                  Navigator.of(context).pushNamed("/playGameFirestoreOnline");
+                                },
+                                //UIの設定
+                                child: Card(
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                                  child: Container(
+                                    padding: EdgeInsets.only(left: 10, top: 10, right: 10), height: 40,
+                                    color: Colors.white,
+                                    child: Text(
+                                      "ルーム${index + 1}（デフォルト）",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.black,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                          );
-                        }
-                        )
-                    ),
-                  ),
+                              );
+                            }
+                            )
+                        ),
+                      ),
 
-              )
-            )
-          ],
-      ),
-    ),
+                    )
+                )
+              ],
+            ),
+          );
+        },
+      )
     );
   }
 }
